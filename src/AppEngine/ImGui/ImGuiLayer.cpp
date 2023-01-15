b6ff1832558f434f1a10d7e197abd8ae68d13cc2
sic_b0y <m.spandel86@gmail.com>
2023-01-15 22:53:31 +0100

    added missing code in imguilayer::end() method

--- src/AppEngine/ImGui/ImGuiLayer.cpp
+++ src/AppEngine/ImGui/ImGuiLayer.cpp
@@ -25,8 +25,16 @@ namespace AppEngine{
    }
 
    void ImGuiLayer::End(){
+      ImGuiIO& io = ImGui::GetIO();
       ImGui::Render();
       ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+      if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		{
+			GLFWwindow* backup_current_context = glfwGetCurrentContext();
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+			glfwMakeContextCurrent(backup_current_context);
+		}
    }
 
    void ImGuiLayer::OnAttach() {
@@ -34,7 +42,9 @@ namespace AppEngine{
       ImGui::CreateContext();
       ImGuiIO& io = ImGui::GetIO(); (void)io;
       io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-      //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls    
+      io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+	
 
       // Setup Dear ImGui style
       ImGui::StyleColorsDark();
@@ -44,7 +54,12 @@ namespace AppEngine{
       Application& app = Application::Get();
       GLFWwindow* window = static_cast<GLFWwindow*>(app.GetWindow().GetNativeWindow());
       ImGui_ImplGlfw_InitForOpenGL(window, true);
-      if (ImGui_ImplOpenGL3_Init("#version 410") == false)
+      #ifdef APP_PLATFORM_LINUX
+         const char* glVersion = "#version 130";
+      #else
+         const char* glVersion = "#version 410";
+      #endif   
+      if (ImGui_ImplOpenGL3_Init(glVersion) == false)
          LOG_ERROR("Could not init opengl3");
    }
 
