91d72749379206f4359b90f9c3f2f6e5e72c437a
Matthias Spandel <m.spandel86@gmail.com>
2023-01-15 20:07:25 +0100

    fixed GLFW premake file for macos

--- vendor/GLFW/premake5.lua
+++ vendor/GLFW/premake5.lua
@@ -1,7 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "off"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -22,6 +22,7 @@ project "GLFW"
 	filter "system:macosx"
 		pic "On"
 		systemversion "latest"
+      staticruntime "On"
 
 		files
 		{
