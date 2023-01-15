540dc7e21598760c5fbf6d27a154627cd7a91631
Matthias <m.spandel86@gmail.com>
2023-01-15 23:07:21 +0100

    corrected ImGuiBuild.cpp

--- src/AppEngine/ImGui/ImGuiBuild.cpp
+++ src/AppEngine/ImGui/ImGuiBuild.cpp
@@ -1,3 +1,5 @@
-#define IMGUI_IMPL_OPENGL_LOADER_GLAD
-// #include <examples/imgui_impl_opengl3.cpp>
-// #include <examples/imgui_impl_glfw.cpp>
+#ifndef IMGUI_IMPL_OPENGL_LOADER_GLAD
+	#define IMGUI_IMPL_OPENGL_LOADER_GLAD
+#endif
+#include <backends/imgui_impl_opengl3.cpp>
+#include <backends/imgui_impl_glfw.cpp>
--- src/ImGuiBuild.cpp
+++
@@ -1,8 +0,0 @@
-
-
-#ifndef IMGUI_IMPL_OPENGL_LOADER_GLAD
-   #define IMGUI_IMPL_OPENGL_LOADER_GLAD
-#endif
-
-#include "backends/imgui_impl_glfw.cpp"
-#include "backends/imgui_impl_opengl3.cpp"
